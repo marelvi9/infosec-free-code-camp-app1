3d036a6e59eac9cadccfc528b48c3883900b737c
marelvi <marelvi.lyg@gmail.com>
2025-12-16 12:38:26 -0300

    Hide X-Powered-By header

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
+app.use(helmet.hidePoweredBy());
 
+const PORT = process.env.PORT || 3030;
+
+
+app.listen(PORT, () => {
+  console.log(`Useful Programmer Info Security App Started on port ${PORT}`);
+});
 
 
 
