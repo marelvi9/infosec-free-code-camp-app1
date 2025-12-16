08d084536988875979b289309cf23be2a3c6421a
marelvi <marelvi.lyg@gmail.com>
2025-12-16 14:31:33 -0300

    Add Content Security Policy

--- myApp.js
+++ myApp.js
@@ -17,6 +17,14 @@ app.use(
 );
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"]
+    }
+  })
+);
 
 
 
