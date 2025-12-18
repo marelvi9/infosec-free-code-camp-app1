b861e074901b69dcbb69cdfcc159c70e54456a61
marelvi <marelvi.lyg@gmail.com>
2025-12-18 17:09:53 -0300

    add hidepower

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+app.use(helmet.hidePoweredBy());
 
 app.use(
   helmet({
     frameguard: { action: "deny" },
+    
 
     contentSecurityPolicy: {
       directives: {
